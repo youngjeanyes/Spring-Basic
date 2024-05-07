ed29e22f14e34ab6b1282ee328c0e3e72cdae9b0
YoungJean Noh <youngjean.noh@riman.com>
2024-05-07 17:19:11 +0900

    class: Applying Object-Oriented Principles

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -16,10 +16,8 @@ class RateDiscountPolicyTest {
     public void vip_o() {
         //given
         Member member = new Member(1L, "memberVIP", Grade.VIP);
-
         //when
         int discount = discountPolicy.discount(member, 10000);
-
         //then
         assertThat(discount).isEqualTo(1000);
     }
@@ -30,10 +28,8 @@ class RateDiscountPolicyTest {
     public void vip_x() {
         //given
         Member member = new Member(2L, "memberBASIC", Grade.BASIC);
-
         //when
         int discount = discountPolicy.discount(member, 10000);
-
         //then
         assertThat(discount).isEqualTo(0);
     }
